1b406f301a8f166a9545b39bab3a09e029d47648
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-04-16 11:05:38 +0200

    App HWND detecting

--- InputSimulator/MainWindow.xaml.cs
+++ InputSimulator/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,17 +13,52 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace InputSimulator
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
   public partial class MainWindow : Window
   {
+    // Import functions from user32.dll
+    [DllImport("user32.dll")]
+    static extern IntPtr GetForegroundWindow();
+
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+    [DllImport("user32.dll")]
+    static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+    private readonly DispatcherTimer _timer;
+
     public MainWindow()
     {
       InitializeComponent();
+
+      // Timer will tick every second
+      _timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+      _timer.Tick += UpdateActiveWindowInfo;
+      _timer.Start();
+    }
+
+    private void UpdateActiveWindowInfo(object sender, EventArgs e)
+    {
+      // Get handle of the active window
+      IntPtr hWnd = GetForegroundWindow();
+
+      // Get the window title
+      var sb = new StringBuilder(256);
+      GetWindowText(hWnd, sb, sb.Capacity);
+      string title = sb.ToString();
+
+      // Get process ID (PID) and thread ID (TID)
+      uint pid;
+      uint tid = GetWindowThreadProcessId(hWnd, out pid);
+
+      // Update the UI
+      TitleText.Text = $"Title: {title}";
+      HandleText.Text = $"HWND: 0x{hWnd.ToInt64():X}";
+      ProcessText.Text = $"PID: {pid}, TID: {tid}";
     }
   }
 }
